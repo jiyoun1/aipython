6116aa0ed08800c6525e0d0e583cc1d1d1513710
jiyoun1 <154905037+jiyoun1@users.noreply.github.com>
2024-01-05 12:06:28 +0900

    Add files via upload

---
+++ 240105.py
@@ -0,0 +1,65 @@
+#words = ['Geeks','for','Geeks']
+#sentence = '-'.join(words)
+#print(sentence)
+
+#str = 'Geeks for Geeks'
+#lines = str.split(' ')
+#print(lines)
+
+
+
+#new_list = [x**2 for x in range(1,6) if x%2 ==0 ]
+#print(new_list)
+
+#city = ["Seoul","New York", "London","Shanghai","Paris","Tokyo"]
+#new_list = [x for x in city if 'S' in city]
+#print(new_list)
+
+#fruits = ['apple','banana','cherry']
+#for index,fruit in enumerate(fruits,start=3):
+#    print(index,fruit)
+
+#fruits = ['a','b','c']
+#for index,fruit in enumerate(fruits):
+#    print("fruit",index,"name is",fruit)
+
+#fruits = ['a','b','c']
+#for x in range(3):
+#    print("fruit",x,"name is",fruits[x])    
+
+#names = ['John','Jane','Doe']
+#ages = [25,30,35]
+
+#for name,age in zip(names,ages):
+#    print(f"{name} is {age} years old")        
+
+#name = ['M','N','S','A']
+#roll_no = [4,1,3,2]
+#A = zip(name,roll_no)
+#B = set(A) 
+#print(B)
+
+#add = lambda x,y : x+y
+
+#print(add(1,2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
